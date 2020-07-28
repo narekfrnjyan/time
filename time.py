74c08fc0aa966f9eb959a447b6c1bf69c0adc203
narekfrnjyan <66741798+narekfrnjyan@users.noreply.github.com>
2020-07-28 20:12:37 +0400

    Update time.py

--- time.py
+++ time.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from sys import argv
+from pytz import timezone
 import pytz
 import time
 if len(argv) >1 and argv[1]=='1':
@@ -10,10 +11,7 @@ if len(argv)>1 and argv[1] == '2':
     print(d)
 if len(argv) > 1 and argv[1] == '3':
     armenia = timezone('asia/yerevan')
-    loc_dt = eastern.localize(datetime.now())
-
+    loc_dt = armenia.localize(datetime.now())
     arm_dt=loc_dt.astimezone(armenia)   
-    fmt = '%Y-%m-%d %H:%M:%S %z'
+    fmt = '%a %b %Y %H:%M:%S %z'
     print(arm_dt.strftime(fmt))
-if len(argv) > 1 and argv[1] == '4':
-    d=datetime.today().strftime('%a %b %Y %H:%M:%S')
